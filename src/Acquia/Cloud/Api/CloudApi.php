fd698574fad578a327dc88dcb3f043f9d659b69e
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2013-09-25 16:52:07 -0400

    Added a bunch of API calls.

--- src/Acquia/Cloud/Api/CloudApi.php
+++ src/Acquia/Cloud/Api/CloudApi.php
@@ -4,6 +4,7 @@ namespace Acquia\Cloud\Api;
 
 use Guzzle\Common\Collection;
 use Guzzle\Http\Client;
+use Zend\Json\Json;
 
 class CloudApi extends Client
 {
@@ -42,13 +43,23 @@ class CloudApi extends Client
     }
 
     /**
-     * Helper method to send a get request and return parsed JSON.
+     * Encodes JSON.
      *
-     * The variables passed in the second parameter are used to expand the URI
-     * expressions, which are usually the resource identifiers being requested.
+     * @param smixed $data
+     *
+     * @return string
+     */
+    public static function jsonEncode($data)
+    {
+        return Json::prettyPrint(Json::encode($data), array('indent' => '    '));
+    }
+
+    /**
+     * Helper method to send a GET request and return parsed JSON.
      *
      * @param string $path
      * @param array $variables
+     *   Variables used to expand the URI expressions.
      *
      * @return array
      *
@@ -62,8 +73,51 @@ class CloudApi extends Client
     }
 
     /**
-     * List all sites accessible by the caller.
+     * Helper method to send a POST request and return parsed JSON.
      *
+     * The variables passed in the second parameter are used to expand the URI
+     * expressions, which are usually the resource identifiers being requested.
+     *
+     * @param string $path
+     * @param array $variables
+     *   Variables used to expand the URI expressions.
+     * @param mixed $body
+     *   Defaults to null. If a non-string is passed then the data is converted
+     *   to JSON.
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @see http://guzzlephp.org/http-client/uri-templates.html
+     */
+    public function sendPost($path, $variables = array(), $body = null)
+    {
+        if (!is_string($body)) {
+            $body = self::jsonEncode($body);
+        }
+        return $this->post(array($path, $variables), null, $body)->send()->json();
+    }
+
+    /**
+     * Helper method to send a DELETE request and return parsed JSON.
+     *
+     * @param string $path
+     * @param array $variables
+     *   Variables used to expand the URI expressions.
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @see http://guzzlephp.org/http-client/uri-templates.html
+     */
+    public function sendDelete($path, $variables = array())
+    {
+        return $this->delete(array($path, $variables))->send()->json();
+    }
+
+    /**
      * @return array
      *
      * @throws \Guzzle\Http\Exception\ClientErrorResponseException
@@ -74,8 +128,6 @@ class CloudApi extends Client
     }
 
     /**
-     * List all sites accessible by the caller.
-     *
      * @param string $site
      *
      * @return array
@@ -89,8 +141,249 @@ class CloudApi extends Client
     }
 
     /**
-     * List all sites accessible by the caller.
+     * @param string $site
+     * @param string $env
      *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function environments($site, $env)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function environment($site, $env, $type)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'type' => $type,
+        );
+        return $this->sendPost('{+base_path}/sites/{site}/envs/{env}/install/{type}.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $environment
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function installDistro($site, $environment)
+    {
+        $variables = array(
+            'site' => $site,
+            'environment' => $environment,
+        );
+        return $this->sendPost('{+base_path}/sites/{site}/envs.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function servers($site, $env)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/servers.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     * @param string $server
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function server($site, $env, $server)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'server' => $server,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/servers/{server}.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     * @param string $server
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function maxPhpProcs($site, $env, $server)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'server' => $server,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/servers/{server}/php-procs.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function sshKeys($site)
+    {
+        $variables = array('site' => $site);
+        return $this->sendGet('{+base_path}/sites/{site}/sshkeys.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param int $id
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function sshKey($site, $id)
+    {
+        $variables = array(
+            'site' => $site,
+            'id' => $id,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/sshkeys/{id}.json', $variables);
+    }
+
+    /**
+     * @param type $site
+     * @param type $publicKey
+     * @param type $nickname
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function addSshKey($site, $publicKey, $nickname)
+    {
+        $path = '{+base_path}/sites/{site}/sshkeys.json?nickname={nickname}';
+        $variables = array(
+            'site' => $site,
+            'nickname' => $nickname,
+        );
+        $body = array('ssh_pub_key' => $publicKey);
+        return $this->sendPost($path, $variables, $body);
+    }
+
+    /**
+     * @param string $site
+     * @param int $id
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function deleteSshKey($site, $id)
+    {
+        $variables = array(
+            'site' => $site,
+            'id' => $id,
+        );
+        return $this->sendDelete('{+base_path}/sites/{site}/sshkeys/{id}.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function svnUsers($site)
+    {
+        $variables = array('site' => $site);
+        return $this->sendGet('{+base_path}/sites/{site}/svnusers.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function svnUser($site, $id)
+    {
+        $variables = array(
+            'site' => $site,
+            'id' => $id,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/svnusers/{id}.json', $variables);
+    }
+
+    /**
+     * @param type $site
+     * @param type $publicKey
+     * @param type $nickname
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @todo Testing returned a 400 response.
+     */
+    public function addSvnUser($site, $username, $password)
+    {
+        $path = '{+base_path}/sites/{site}/svnusers/{username}.json';
+        $variables = array(
+            'site' => $site,
+            'username' => $username,
+        );
+        $body = array('password' => $password);
+        return $this->sendPost($path, $variables, $body);
+    }
+
+    /**
+     * @param string $site
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     *
+     * @todo Testing returned a 400 response.
+     */
+    public function deleteSvnUser($site, $id)
+    {
+        $variables = array(
+            'site' => $site,
+            'id' => $id,
+        );
+        return $this->sendDelete('{+base_path}/sites/{site}/svnusers/{id}.json', $variables);
+    }
+
+    /**
      * @param string $site
      *
      * @return array
@@ -104,36 +397,117 @@ class CloudApi extends Client
     }
 
     /**
-     * List all sites accessible by the caller.
-     *
      * @param string $site
+     * @param string $db
      *
      * @return array
      *
      * @throws \Guzzle\Http\Exception\ClientErrorResponseException
      */
-    public function siteDatabase($site, $database)
+    public function siteDatabase($site, $db)
     {
         $variables = array(
             'site' => $site,
-            'database' => $database,
+            'db' => $db,
         );
-        return $this->sendGet('{+base_path}/sites/{site}/dbs/{database}.json', $variables);
+        return $this->sendGet('{+base_path}/sites/{site}/dbs/{db}.json', $variables);
     }
 
     /**
-     * List all sites accessible by the caller.
-     *
      * @param string $site
+     * @param string $env
      *
      * @return array
      *
      * @throws \Guzzle\Http\Exception\ClientErrorResponseException
      */
-    public function environments($site)
+    public function environmentDatabases($site, $env)
     {
-        $variables = array('site' => $site);
-        return $this->sendGet('{+base_path}/sites/{site}/envs.json', $variables);
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/dbs.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     * @param string $db
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function environmentDatabase($site, $env, $db)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'db' => $db,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/dbs/{db}.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     * @param string $db
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function dabaseBackups($site, $env, $db)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'db' => $db,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/dbs/{db}/backups.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     * @param string $db
+     * @param string $id
+     *
+     * @return array
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function dabaseBackup($site, $env, $db, $id)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'db' => $db,
+            'id' => $id,
+        );
+        return $this->sendGet('{+base_path}/sites/{site}/envs/{env}/dbs/{db}/backups/{id}.json', $variables);
+    }
+
+    /**
+     * @param string $site
+     * @param string $env
+     * @param string $db
+     * @param string $id
+     *
+     * @return \Guzzle\Http\Message\Response
+     *
+     * @throws \Guzzle\Http\Exception\ClientErrorResponseException
+     */
+    public function downloadDabaseBackup($site, $env, $db, $id)
+    {
+        $variables = array(
+            'site' => $site,
+            'env' => $env,
+            'db' => $db,
+            'id' => $id,
+        );
+        return $this->get('{+base_path}/sites/{site}/envs/{env}/dbs/{db}/backups/{id}/download.json', $variables)->send();
     }
 
     /**
@@ -144,7 +518,7 @@ class CloudApi extends Client
      */
     public function asJson($filename = null)
     {
-        $data = array(
+        $data = self::jsonEncode(array(
             'services' => array(
                 'cloud_api' => array(
                     'class' => get_class($this),
@@ -154,17 +528,7 @@ class CloudApi extends Client
                     ),
                 ),
             ),
-        );
-
-        $options = JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT;
-        if (defined('JSON_PRETTY_PRINT')) {
-            $options = $options | JSON_PRETTY_PRINT;
-        }
-        if (defined('JSON_UNESCAPED_SLASHES')) {
-            $options = $options | JSON_UNESCAPED_SLASHES;
-        }
-
-        $json = json_encode($data, $options);
+        ));
 
         if ($filename !== null) {
             file_put_contents($filename, $json);
